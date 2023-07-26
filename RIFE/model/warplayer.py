e392f6f6203b339d2e5efb30da2ad17ca8c47447
LKV <87376138+ZhengQinLai@users.noreply.github.com>
2023-07-26 13:36:04 +0800

    Update warplayer.py-device

--- RIFE/model/warplayer.py
+++ RIFE/model/warplayer.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cpu")
 backwarp_tenGrid = {}
 
 
